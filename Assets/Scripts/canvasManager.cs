7022516cf41ed6740cee2617a5e0bb81993907dd
WayneZhu <waynezhu3188@gmail.com>
2020-09-26 12:37:41 -0700

    Hi

--- Assets/Scripts/canvasManager.cs
+++ Assets/Scripts/canvasManager.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class canvasManager : MonoBehaviour
 {
+    public GameObject FadePanel;
+
+    private void Start()
+    {
+        FadePanel.SetActive(false);
+    }
     public void QuitGame()
     {
         Application.Quit();
@@ -12,11 +19,23 @@ public class canvasManager : MonoBehaviour
 
     public void ResetLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutin(FadeEffect(SceneManager.GetActiveScene().buildIndex));
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void NextLevel()
     {
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex+1)%SceneManager.sceneCountInBuildSettings);
+        StartCoroutin(FadeEffect(SceneManager.GetActiveScene().buildIndex+1));
+        //SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex+1)%SceneManager.sceneCountInBuildSettings);
+    }
+
+    IEnumerator FadeEffect(int SceneToLoad)
+    {
+        object p = FadePanel.SetActive(true);
+        for (int i = 0; i < 100; i++)
+        {
+            FadePanel.GetComponent < CanvasGroup().alpha = (float)i * 0.01f;
+            yield return new WaitForSecondsRealtime(0.01f);
+        }
     }
 }
